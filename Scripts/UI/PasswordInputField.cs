9e4a1a5878190f9380dca417524c33f8b6d1408a
Aleksandar Jovanović <aleksandarjovanovic@elfak.rs>
2019-10-06 23:00:44 +0200

    Add initial script files for VoRtex platform.
    <Added files include networking, blockchain and virtual worlds logic.
    Files will be refactored and updated in the next version.

---
+++ Scripts/UI/PasswordInputField.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordInputField : MonoBehaviour {
+
+    public InputField confirmPasswordInputField;
+    public Button createWalletButton;
+
+    private bool m_passwordConfirmed = false;
+
+    public bool passwordConfirmed()
+    {
+        return m_passwordConfirmed;
+    }
+
+    public void resetFields()
+    {
+        gameObject.GetComponent<InputField>().text = "";
+        confirmPasswordInputField.text = "";
+    }
+
+    public string passwordString()
+    {
+        return gameObject.GetComponent<InputField>().text;
+    }
+
+    public void validatePasswordConfirmation()
+    {
+        m_passwordConfirmed = passwordString().Equals(confirmPasswordInputField.text);
+        createWalletButton.interactable = m_passwordConfirmed;
+    }
+}
