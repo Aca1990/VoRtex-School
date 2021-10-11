6631d426aadc1ce6d1159573bd540c657849d305
Aleksandar Jovanović <aleksandarjovanovic@elfak.rs>
2021-10-11 21:38:51 +0200

    Add files via upload

---
+++ Scripts/ServerDataManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerDataManager : MonoBehaviour
+{
+    private List<string> listOfActiveAchievements= new List<string> { "ProffesorVortexChat","MoveObject", "InteractWithUsers"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DBManager.LoggedIn)
+        {
+            Debug.Log("Welcome " + DBManager.username);
+            Debug.Log("What have you achieve so far: " + DBManager.achievements);
+
+            foreach (var a in DBManager.achievements)
+            {
+                if(listOfActiveAchievements.Contains(a))
+                {
+                    Debug.Log(a);
+                }
+            }
+        }
+    }
+}
